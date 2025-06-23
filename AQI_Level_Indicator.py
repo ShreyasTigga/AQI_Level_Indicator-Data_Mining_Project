3578a8018071c333a90cda93deb38975bd5dcf8a
Shreyas Vaibhav Tigga <139065006+ShreyasTigga@users.noreply.github.com>
2025-06-23 22:58:28 +0530

    Create AQI_Level_Indicator.py

---
+++ AQI_Level_Indicator.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier, export_text, plot_tree
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+
+print("Hello World!")
